2fa58bccee8622af07e2d9786fca3d07dd1096bf
Fuzss <28218241+Fuzss@users.noreply.github.com>
2023-01-28 00:23:10 +0100

    test mixinconfigplugin

--- Fabric/src/main/java/fuzs/enderzoology/mixin/EnchantmentCategoryFabricMixin.java
+++ Fabric/src/main/java/fuzs/enderzoology/mixin/EnchantmentCategoryFabricMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 // this is the CROSSBOW enchantment category, allow the hunting bow to receive these kinds of enchantments, too
 // much better implementation on Forge via dedicated method override on hunting bow item class
 @Mixin(targets = "net.minecraft.world.item.enchantment.EnchantmentCategory$13")
-abstract class EnchantmentCategoryMixin {
+abstract class EnchantmentCategoryFabricMixin {
 
     @Inject(method = "canEnchant", at = @At("HEAD"), cancellable = true)
     public void canEnchant(Item item, CallbackInfoReturnable<Boolean> callback) {
