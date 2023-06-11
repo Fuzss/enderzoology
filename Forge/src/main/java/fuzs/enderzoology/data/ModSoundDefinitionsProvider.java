b7c9951a35144717200e8a36082c7cb9bb5a96b9
Fuzss <28218241+Fuzss@users.noreply.github.com>
2023-06-11 14:55:14 +0200

    fix license

--- Forge/src/main/java/fuzs/enderzoology/data/ModSoundDefinitionsProvider.java
+++ Forge/src/main/java/fuzs/enderzoology/data/ModSoundDefinitionsProvider.java
@@ -18,9 +18,9 @@ public class ModSoundDefinitionsProvider extends AbstractSoundDefinitionProvider
         this.add(ModRegistry.DIRE_WOLF_DEATH_SOUND_EVENT.get(), sound(this.id("mob/dire_wolf/death")));
         this.add(ModRegistry.DIRE_WOLF_GROWL_SOUND_EVENT.get(), sound(this.id("mob/dire_wolf/growl1")), sound(this.id("mob/dire_wolf/growl2")), sound(this.id("mob/dire_wolf/growl3")));
         this.add(ModRegistry.DIRE_WOLF_HOWL_SOUND_EVENT.get(), sound(this.id("mob/dire_wolf/howl")));
-        this.add(ModRegistry.OWL_HOOT_SOUND_EVENT.get(), sound(this.id("mob/owl/hoot1")), sound(this.id("mob/owl/hoot2")));
-        this.add(ModRegistry.OWL_HURT_SOUND_EVENT.get(), sound(this.id("mob/owl/hurt")));
-        this.add(ModRegistry.OWL_DEATH_SOUND_EVENT.get(), sound(this.id("mob/owl/hurt")));
+        this.add(ModRegistry.OWL_HOOT_SOUND_EVENT.get(), sound(this.id("mob/owl/hoot1")).volume(0.2F), sound(this.id("mob/owl/hoot2")).volume(0.2F));
+        this.add(ModRegistry.OWL_HURT_SOUND_EVENT.get(), sound(this.id("mob/owl/hurt")).volume(0.2F));
+        this.add(ModRegistry.OWL_DEATH_SOUND_EVENT.get(), sound(this.id("mob/owl/hurt")).volume(0.2F));
         this.add(ModRegistry.OWL_EGG_THROW_SOUND_EVENT.get(), sound(SoundEvents.EGG_THROW));
     }
 }
